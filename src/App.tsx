ab3fc3bc71fcf841d26680c644a1cea3b007d4b8
MaximilianAdF <MaximilianAdF@users.noreply.github.com>
2025-10-22 15:17:53 +0200

    Create react project, ts, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-8 text-center">
+      <h1 className="text-4xl font-bold">Hello Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
